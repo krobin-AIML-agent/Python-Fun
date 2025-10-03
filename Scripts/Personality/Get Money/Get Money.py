85fd0d7c5c473ff49d4729efc12b9c3538217e3d
Kevin Robinson <kevinrobinson8241997@gmail.com>
2025-10-02 21:00:38 -0400

    Add files via upload

---
+++ Scripts/Personality/Get Money/Get Money.py
@@ -0,0 +1,15 @@
+# %%
+Munyun = int(input("How much money you got right now? "))
+target = 100000
+
+if Munyun < target:
+    print("Go get some money!")
+else:
+    print("Go get some money!")
+
+
+
+
+
+
+
